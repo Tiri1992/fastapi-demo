e3c544111498bcf83803a896f6406410832b1bcc
Sotiri Georgiou <tirigeorgiou@gmail.com>
2022-04-17 21:51:10 +0100

    finished adding http status code 201

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from lib2to3.pgen2.token import OP
 from turtle import pos
 from fastapi import FastAPI
+from fastapi import Response
+from fastapi import status
+from fastapi import HTTPException
 from fastapi.params import Body
 # Schema validation with pydantic
 from pydantic import BaseModel
@@ -10,6 +13,8 @@ from random import randrange
 app = FastAPI()
 
 # NOTE: If you have to routes that are the same i.e. '/' it will resolve the first function as the route.
+
+# Curr video time: 2:02:01
 
 
 class Post(BaseModel):
@@ -35,6 +40,12 @@ my_post = [
 ]
 
 
+def find_post(id: int) -> Optional[dict]:
+    for post in my_post:
+        if post["id"] == id:
+            return post
+
+
 @app.get("/")
 def read_root():
     # Path operation (or route)
@@ -50,7 +61,7 @@ def get_posts():
     }
 
 
-@app.post("/posts")
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_post(post: Post):
     # Name of the function never matters, but its bad practice to have
     # the http method inside the endpoint i.e. 'createposts'
@@ -64,4 +75,24 @@ def create_post(post: Post):
     # ALWAYS SEND BACK THE DATA CREATED TO THE CLIENT!
     return {
         "data": post_dict,
+    }
+
+# NOTE: ORDER MATTERS, if I define an endpoint /posts/latest it will work top down to find matching path
+# So this will need to be before /posts/{id} or it will this your id = "latest" and throw an error.
+
+
+@app.get("/posts/{id}")
+def get_post(id: int, response: Response):
+    # posts -> plural
+    # path parameter is the {id} field: we type annotated it here as an int
+    # NOTE: make sure you put the type annotation as int to convert it from str -> int (for id)
+    res = find_post(id)
+    if not res:
+        # Check http status codes. 404 = server can not find the requested resource
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"post with id: {id} was not found.",
+        )
+    return {
+        "post_detail": find_post(id)
     }
