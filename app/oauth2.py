95cf1325486fbbb7d0291fbe67ec87609bb91909
Tiri <tirigeorgiou@gmail.com>
2022-05-16 08:50:21 +0100

    secret key now env variable

--- app/oauth2.py
+++ app/oauth2.py
@@ -5,6 +5,7 @@ from sqlalchemy.orm import Session
 from . import schema
 from . import database
 from . import models
+from .config import settings
 from jose import JWSError, jwt
 from datetime import datetime, timedelta
 
@@ -15,9 +16,9 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 # WE NEED 3 things to sign the token
 # SECRET, HEADER, PAYLOAD
 # STORE SECRET_KEY AS ENV VARIABLE
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 60
+SECRET_KEY = settings.secret_key
+ALGORITHM = settings.algorithm
+ACCESS_TOKEN_EXPIRE_MINUTES = settings.access_token_expire_minutes
 
 
 def create_access_token(data: dict):
