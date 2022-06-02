aa60b00f1914b298c7f108a6c777e789941f733c
Tiri <tirigeorgiou@gmail.com>
2022-06-02 19:57:29 +0100

    corsmiddleware

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from lib2to3.pgen2.token import OP
 from fastapi import FastAPI
+# CORS
+from fastapi.middleware.cors import CORSMiddleware
 # Models
 from . import models
 from .database import engine
@@ -13,6 +15,17 @@ from .routers import post, user, auth, vote
 # Create an instance
 app = FastAPI()
 
+# Best security practice to narrow down the origins that should
+# have access to our application
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # NOTE: If you have to routes that are the same i.e. '/' it will resolve the first function as the route.
 
@@ -22,3 +35,10 @@ app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
 app.include_router(vote.router)
+
+
+@app.get('/')
+def root():
+    return {
+        "message": "Hello World!"
+    }
